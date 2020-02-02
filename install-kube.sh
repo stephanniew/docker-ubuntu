3cf88935159a6c708e63d3f31153e3897faec10e
stephanniew <59134423+stephanniew@users.noreply.github.com>
2020-02-02 11:21:26 +0100

    Update install-kube.sh

--- install-kube.sh
+++ install-kube.sh
@@ -1,8 +1,8 @@
 # docker ce
-apt-get update && apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt-get update && apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update && apt-get install docker-ce docker-ce-cli containerd.io
+apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # docker compse
 curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -16,6 +16,3 @@ EOF
 apt-get update && apt-get install -y kubelet kubeadm kubectl && apt-mark hold kubelet kubeadm kubectl
 
 
-
-curl -fsSL https://get.docker.com -o install.sh
-sh install.sh
