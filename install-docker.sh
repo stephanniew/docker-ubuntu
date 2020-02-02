a18167bf2fad1c590cdde7f044758383349063a0
stephanniew <59134423+stephanniew@users.noreply.github.com>
2020-02-02 11:07:17 +0100

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,8 +1,8 @@
 # docker ce
-apt-get update && apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt-get update && apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update && apt-get install docker-ce docker-ce-cli containerd.io
+apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # test docker network
 docker network create --driver=bridge --subnet=172.30.0.0/16 test
